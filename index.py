65274b91b5d7173fa904cedb1c27fd4ec29809f7
Karen Pilosyan <pilosyan.karen@gmail.com>
2020-03-04 15:35:59 +0400

    Updates

--- index.py
+++ index.py
@@ -10,13 +10,11 @@ def getSideCategoryNames(url):
     categories = []
     sauce = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(sauce, "html.parser")
-    categories_tag = soup.find(class_ = "side_categories").strings
+    categories_tag = soup.find(class_ = "side_categories").stripped_strings
 
     for i in categories_tag:
         categories.append(i)
     
-    categories = [s.strip() for s in categories]    # cleaning the
-    categories = [i for i in categories if i]       # list 
     categories.remove("Books")                      # Removing 'Books' category which includes all books 
     return categories
 
@@ -24,19 +22,23 @@ def getCategoryPagesUrls(categoryName):
     sauce = urllib.request.urlopen(main_url).read()
     soup = BeautifulSoup(sauce, "html.parser")
     urls = []
-    if soup.find(text = categoryName):
-        tag = soup.find(text = categoryName).parent 
-        urls.append(tag["href"])
-        sauce = urllib.request.urlopen(urls[0]).read()
-        soup = BeautifulSoup(sauce, "html.parser")
-
-        count = 1
-        while soup.find(text ="next"):
-            tag = soup.find(text = "next").parent 
-            urls.append(tag["href"])
-            sauce = urllib.request.urlopen(urls[count]).read()
+    for i in soup.find_all("a"):
+        if i.get_text(strip=True) == categoryName:
+            tag = i
+            first_url = main_url + str(tag["href"])
+            urls.append(first_url)
+            print(urls[0])
+            sauce = urllib.request.urlopen(urls[0]).read()
             soup = BeautifulSoup(sauce, "html.parser")
-            count += 1
+
+            count = 1
+            while soup.find(text ="next"):
+                tag = soup.find(text = "next").parent 
+                next_url = first_url[:-10] + str(tag["href"])
+                urls.append(next_url)
+                sauce = urllib.request.urlopen(urls[count]).read()
+                soup = BeautifulSoup(sauce, "html.parser")
+                count += 1
 
     return urls
 
